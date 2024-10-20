6800153a4c1a1fed034e4c972f26c3c0464c5500
Tomasrv333 <tomasrv123@gmail.com>
2024-10-20 13:10:03 -0500

    update: Logout button header working

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Cookies from 'js-cookie';
+import { useRouter } from 'next/navigation';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Header = ({ onToggleSidebar, isSidebarOpen }) => {
+  const router = useRouter();
+
+  function handleLogout() {
+    Cookies.remove('token');
+    router.push('/pages/login');
+  }
+
   return (
     <div className='flex justify-between items-center bg-primary text-white px-6 py-2'>
         <div className='flex items-center gap-6'>
@@ -14,7 +23,10 @@ const Header = ({ onToggleSidebar, isSidebarOpen }) => {
             />
             <h1 className='font-bold text-lg'>Funlo</h1>
         </div>
-        <input type='text' placeholder='Buscar'></input>
+        <div className='flex items-center gap-4'>
+          <input type='text' placeholder='Buscar'></input>
+          <button type='button' onClick={handleLogout}>Cerrar sesión</button>
+        </div>
     </div>
   )
 }
